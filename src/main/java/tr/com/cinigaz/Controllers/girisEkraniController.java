57e6b0fd1873e609efa2bc02480e20d31b5d3f72
eda <eda@gmail.com>
2020-03-04 18:41:03 +0300

    aboneBilgiEkrani girisEkrani eklendi.

--- src/main/java/tr/com/cinigaz/Controllers/girisEkraniController.java
+++ src/main/java/tr/com/cinigaz/Controllers/girisEkraniController.java
@@ -20,30 +20,51 @@ public class girisEkraniController {
 
     @FXML
     private void kartOkumaAc(ActionEvent kartOkumaEvent) {
-        // onceki ekranı kapat.
-        Stage stage2;
-        stage2 = (Stage) kartOkumaAcBtn.getScene().getWindow();
-        stage2.hide();
-        // Burada kart bilgilieri alınbmay çalışılacak :: // Özgür
-        // Soraki ekrana karar ver
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/kartOkumaRet.fxml"));     // Bu kart tanımsız
-        // veya
-        //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/aboneBilgiEkranı.fxml")); // Abone bilgilerinin gösterildiği ekran
-        Parent root1;
+        FXMLLoader loader = new FXMLLoader();
+
+
+        loader.setLocation(girisEkraniController.class.getClassLoader().getResource("/views/aboneBilgileriController.fxml"));
+        //  FXMLLoader fxmlLoader3 = new FXMLLoader(getClass().getResource("/views/aboneBilgileriController.fxml"));
+        Parent root3;
         try {
-            root1 = (Parent) fxmlLoader.load();
+            root3 = (Parent) loader.load();
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.initStyle(StageStyle.UNDECORATED);
-            stage.setTitle("ÇİNİGAZ DOĞALGAZ KİOSK FATURA ODEME");
-
-            stage.setScene(new Scene(root1));
+            stage.setScene(new Scene(root3));
             stage.show();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+
         }
     }
+
+
+//        // onceki ekranı kapat.
+//        Stage stage2;
+//        stage2 = (Stage) kartOkumaAcBtn.getScene().getWindow();
+//        stage2.hide();
+//        // Burada kart bilgilieri alınbmay çalışılacak :: // Özgür
+//        // Soraki ekrana karar ver
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/kartOkumaRet.fxml"));     // Bu kart tanımsız
+//        // veya
+//        //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/views/aboneBilgiEkranı.fxml")); // Abone bilgilerinin gösterildiği ekran
+//        Parent root1;
+//        try {
+//            root1 = (Parent) fxmlLoader.load();
+//            Stage stage = new Stage();
+//            stage.initModality(Modality.APPLICATION_MODAL);
+//            stage.initStyle(StageStyle.UNDECORATED);
+//            stage.setTitle("ÇİNİGAZ DOĞALGAZ KİOSK FATURA ODEME");
+//
+//            stage.setScene(new Scene(root1));
+//            stage.show();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
